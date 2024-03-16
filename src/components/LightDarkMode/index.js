60cdf7492cb76350585be136814a4e23e7542e29
krishnatutorial <krishnakjp963@gmail.com>
2024-03-16 04:54:09 +0000

    Code giving error

---
+++ src/components/LightDarkMode/index.js
@@ -0,0 +1,16 @@
+// Write your code here
+import {Component} from 'react'
+
+import './index.css'
+
+class LightDarkMode extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Click To Change Mode</h1>
+        <button>Light Mode</button>
+      </div>
+    )
+  }
+}
+export default LightDarkMode
